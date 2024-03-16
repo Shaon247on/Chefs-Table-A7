aa366d03c6c170540b0b0a5df39b87d5c53b78eb
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-03-16 12:31:17 +0600

    trying to solve time and fire icon issue

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-const Card = ({card,handleToCook}) => {
+const Card = ({ card, handleToCook }) => {
     const { recipe_name, short_description, ingredients, preparing_time, calories, recipe_image } = card
     return (
         <div>
@@ -15,22 +15,29 @@ const Card = ({card,handleToCook}) => {
                     <div>
                         <h4 className="text-lg font-medium">Ingredients: {ingredients.length}</h4>
                         <ul className="ml-6 list-disc font-fira text-[#878787] mb-8">
-                           {
-                            ingredients.map((ingredient, idx) => <li key={idx}>{ingredient}</li>)
-                           } 
+                            {
+                                ingredients.map((ingredient, idx) => <li key={idx}>{ingredient}</li>)
+                            }
                         </ul>
 
                         <div className="flex gap-4 mb-6">
                             <div className="flex gap-1 items-center font-fira text-[#282828CC]">
-                                <img src="src/assets/image/Clock.svg" alt=""></img>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+
                                 <p>{preparing_time} minutes</p>
                             </div>
                             <div className="flex gap-1 items-center font-fira text-[#282828CC]">
-                                <img src="src/assets/image/Fire.svg" alt=""></img>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
+                                </svg>
+
                                 <p>{calories} calories</p>
                             </div>
                         </div>
-                        <button onClick={()=>handleToCook(card)} className="bg-[#0BE58A] text-[#150B2B] border-none w-32 lg:w-48 py-4 rounded-[30px] text-[15px] lg:text-[20px] font-semibold mb-12">Want to Cook</button>
+                        <button onClick={() => handleToCook(card)} className="bg-[#0BE58A] text-[#150B2B] border-none w-32 lg:w-48 py-4 rounded-[30px] text-[15px] lg:text-[20px] font-semibold mb-12">Want to Cook</button>
                     </div>
                 </div>
             </div>
@@ -38,9 +45,9 @@ const Card = ({card,handleToCook}) => {
     );
 };
 
-Card.propTypes= {
+Card.propTypes = {
     card: PropTypes.object.isRequired,
     handleToCook: PropTypes.func.isRequired,
-    
+
 }
 export default Card;
